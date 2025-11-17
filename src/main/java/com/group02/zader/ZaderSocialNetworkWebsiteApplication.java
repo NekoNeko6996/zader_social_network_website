5326fe1d932219e7050371e101ed38593da57c14
NekoNeko6996 <lqm231231@gmail.com>
2025-11-17 12:26:59 +0700

    first commit

---
+++ src/main/java/com/group02/zader/ZaderSocialNetworkWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.group02.zader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaderSocialNetworkWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaderSocialNetworkWebsiteApplication.class, args);
+	}
+
+}
